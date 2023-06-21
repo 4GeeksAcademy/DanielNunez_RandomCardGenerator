d82ee39ac8ecde156ba4b2dda277d8b2c6342b8c
DanielNunez04 <130614771+DanielNunez04@users.noreply.github.com>
2023-06-21 23:43:17 +0000

    RandomCardGenerator_Daniel

--- src/app.js
+++ src/app.js
@@ -1,11 +1,53 @@
-/* eslint-disable */
-import "bootstrap";
 import "./style.css";
+const numbers = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const symbols = [
+  { symbol: "&spades;", color: "black" },
+  { symbol: "&hearts;", color: "red" },
+  { symbol: "&clubs;", color: "black" },
+  { symbol: "&diams;", color: "red" }
+];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function showRandomCard() {
+  const cardElement = document.getElementById("randomCard");
+  const randomSymbolsIndex = Math.floor(Math.random() * symbols.length);
+  const randomNumberslIndex = Math.floor(Math.random() * numbers.length);
+  const symbolInCard = symbols[randomSymbolsIndex];
+  const numberInCard = numbers[randomNumberslIndex];
+  const upperSymbol = document.getElementById("upperSymbol");
+
+  upperSymbol.innerHTML = symbolInCard.symbol;
+  upperSymbol.style.color = symbolInCard.color;
+  const lowerSymbol = document.getElementById("lowerSymbol");
+
+  lowerSymbol.innerHTML = symbolInCard.symbol;
+  lowerSymbol.style.color = symbolInCard.color;
+  const numberElement = document.getElementById("numbers");
+
+  numberElement.innerHTML = numberInCard;
+  if (symbolInCard.symbol === "&diams;" || symbolInCard.symbol === "&hearts;") {
+    numberElement.style.color = "red";
+  } else {
+    numberElement.style.color = "black";
+  }
+}
+const button = document.getElementById("cardButton");
+
+button.addEventListener("click", showRandomCard);
+
+showRandomCard();
+setInterval(showRandomCard, 1000);
